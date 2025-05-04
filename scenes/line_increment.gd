911eda051eda43d0f0694a9d24990d3c043111f3
Igor Jese <igor@mockupscreens.com>
2025-05-04 18:46:21 +0200

    WorldDialog commands: move, all

--- scenes/line_increment.gd
+++ scenes/line_increment.gd
@@ -5,8 +5,19 @@ signal count_changed(id: String, new_count: int)
 
 var id := ""
 var count := 0
-var cost_mats := 0
-var cost_pers := 0
+var cost_mats := 0 # BUILD 
+var cost_pers := 0 # BUILD 
+var available := 0  # MOVE 
+
+# modes must be same as in WorldDialog
+enum Mode { BUILD, MOVE }
+var current_mode = Mode.BUILD
+
+
+func set_mode(mode: int):
+    current_mode = mode
+    update_gui()
+
 
 
 func setup(line_id: String):
@@ -16,23 +27,37 @@ func setup(line_id: String):
     var design = GameData.ship_designs.get(id)
     cost_mats = design.cost_mats
     cost_pers = design.cost_pers
-    update_count()
+    update_gui()
     
 func reset():
     count = 0
-    update_count()
+    update_gui()
 
 
-func update_count():
+func update_gui():
     $Qty.text = str(count)
-    $Info.text = "Cost: %dM %dP" % [cost_mats * count, cost_pers * count]
+
+    if current_mode == Mode.BUILD:  
+        $Info.text = "Cost: %dM %dP" % [cost_mats * count, cost_pers * count]
+    else:  # MOVE
+        available = hulls_available(id)
+        $Info.text = "%d available" % available
+
     emit_signal("count_changed", id, count)
 
 func _on_decrease_pressed():
     if count > 0:
         count -= 1
-        update_count()
+        update_gui()
 
 func _on_increase_pressed():
     count += 1
-    update_count()
+    update_gui()
+
+
+func hulls_available(hull: String) -> int:
+    var count = 0
+    for ship in GameData.ships:
+        if ship.faction == "player" and ship.location == GameData.selected_world.world_name and ship.type == hull:
+            count += 1
+    return count
--- world_dialog.gd
+++ world_dialog.gd
@@ -4,19 +4,23 @@ extends PopupPanel
 @onready var line_scene = preload("res://scenes/LineIncrement.tscn")
 
 var world = null
-var build_order = {}
+var ship_order = {}
+
+enum Mode { BUILD, MOVE }
+var current_mode = Mode.BUILD
 
 signal ships_built
-
+signal ships_moved
 
 func _ready():
-    build_order.clear()
+    ship_order.clear()
     for hull in GameData.ship_designs.keys():
         var line = line_scene.instantiate()
         line.setup(hull)
         line.connect("count_changed", Callable(self, "_on_line_updated"))
         line_container.add_child(line)
-        build_order[hull] = 0
+        ship_order[hull] = 0
+        $VBox/Cmd/Move.get_popup().connect("id_pressed", Callable(self, "_on_move"))
 
 
 func prepare_for_world(world_node):
@@ -25,46 +29,68 @@ func prepare_for_world(world_node):
 
     for line in line_container.get_children():
         line.reset()  # you'll add this method to set count = 0
-        build_order[line.id] = 0
+        ship_order[line.id] = 0
 
     popup_centered()
 
 
 func _on_line_updated(id: String, count: int):
-    build_order[id] = count
-    update_total_cost()
+    ship_order[id] = count
+    update_gui()
 
 
-func update_total_cost():
+func update_gui():
+    # Sync button state
+    $VBox/Mode/Shipyard.button_pressed = (current_mode == Mode.BUILD)
+    $VBox/Mode/Fleets.button_pressed = (current_mode == Mode.MOVE)
+
+    # Common UI logic
     var total_mats = 0
     var total_pers = 0
+    var total_ships = 0
 
-    for id in build_order.keys():
-        var count = build_order[id]
-        var design = GameData.ship_designs.get(id)
-        if design:
-            total_mats += design.cost_mats * count
-            total_pers += design.cost_pers * count
+    for id in ship_order.keys():
+        var count = ship_order[id]
+        total_ships += count
 
-    $VBox/Summary.text = "Total: %dM  %dP" % [total_mats, total_pers]
+        if current_mode == Mode.BUILD:
+            var design = GameData.ship_designs.get(id)
+            if design:
+                total_mats += design.cost_mats * count
+                total_pers += design.cost_pers * count
+                
+    $VBox/Cmd/Build.visible = (current_mode == Mode.BUILD)
+    $VBox/Cmd/All.visible = (current_mode == Mode.MOVE)
+    $VBox/Cmd/Move.visible = (current_mode == Mode.MOVE)
 
-    $VBox/Cmd/Build.disabled = (
-        total_mats > GameData.player_materials or
-        total_pers > GameData.player_personnel
-    )    
+    if current_mode == Mode.BUILD:
+        $VBox/Summary.text = "Total: %dM  %dP" % [total_mats, total_pers]
+        $VBox/Cmd/Build.disabled = (
+            total_mats > GameData.player_materials or
+            total_pers > GameData.player_personnel or
+            total_ships == 0
+        )
+    elif current_mode == Mode.MOVE:
+        $VBox/Summary.text = "Selected: %d ship(s)" % total_ships
+        $VBox/Cmd/Move.disabled = (total_ships == 0)
+        populate_move_destinations()
 
 
 func _on_clear_pressed():
+    clear_line_items()
+    
+    
+func clear_line_items():
     for line in line_container.get_children():
         if line is LineIncrement:
             line.reset()
-            build_order[line.id] = 0
-    update_total_cost()
+            ship_order[line.id] = 0
+    update_gui()
 
 
 func _on_build_pressed():
-    for id in build_order.keys():
-        var count = build_order[id]
+    for id in ship_order.keys():
+        var count = ship_order[id]
         if count == 0:
             continue
 
@@ -91,3 +117,52 @@ func _on_build_pressed():
 
     hide()
     emit_signal("ships_built")
+
+
+func _on_shipyard_pressed():
+    current_mode = Mode.BUILD
+    switch_mode_for_lines()
+    update_gui()
+
+    
+func _on_fleets_pressed():
+    current_mode = Mode.MOVE
+    switch_mode_for_lines()
+    update_gui()
+    
+func switch_mode_for_lines():
+    clear_line_items()
+    for line in line_container.get_children():
+        if line is LineIncrement:
+            line.set_mode(current_mode)
+            
+func _on_move(id: int):
+    var destination = $VBox/Cmd/Move.get_popup().get_item_text(id)
+    print(destination)
+    hide()
+    emit_signal("ships_moved")
+
+
+func populate_move_destinations():
+    var menu = $VBox/Cmd/Move.get_popup()
+    menu.clear()
+
+    var index = 0
+    for pair in GameData.connections:
+        var other = ""
+        if pair[0] == world.world_name:
+            other = pair[1]
+        elif pair[1] == world.world_name:
+            other = pair[0]
+        else:
+            continue
+
+        menu.add_item(other, index)
+        index += 1
+
+
+func _on_all_pressed():
+    for line in line_container.get_children():
+        if line is LineIncrement and current_mode == Mode.MOVE:
+            line.count = line.available
+            line.update_gui()
